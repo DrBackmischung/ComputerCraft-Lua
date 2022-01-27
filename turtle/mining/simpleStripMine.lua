6481c2c2c637a2c06c1d49f3082a7f6cdf501dc5
Mathis Neunzig <mathis.neunzig@gmail.com>
2022-01-27 14:08:03 +0100

    Update simpleStripMine.lua

--- turtle/mining/simpleStripMine.lua
+++ turtle/mining/simpleStripMine.lua
@@ -1,1 +1,40 @@
+print("Startet Mining...")
+turtle.refuel()
 
+function mine()
+  turtle.refuel()
+  for i=0,3,1 do
+    turtle.dig()
+    turtle.forward()
+    turtle.digUp()
+    turtle.digDown()
+  end
+  turtle.turnLeft()
+  for i=0,5,1 do
+    turtle.dig()
+    turtle.forward()
+    turtle.digUp()
+    turtle.digDown()
+  end
+  turtle.turnLeft()
+  turtle.turnLeft()
+  for i=0,5,1 do
+    turtle.forward()
+  end
+  for i=0,5,1 do
+    turtle.dig()
+    turtle.forward()
+    turtle.digUp()
+    turtle.digDown()
+  end
+  turtle.turnLeft()
+  turtle.turnLeft()
+  for i=0,5,1 do
+    turtle.forward()
+  end
+  turtle.turnRight()
+  mine()
+end
+
+mine()
+  
